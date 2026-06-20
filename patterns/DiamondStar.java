c2483a4f92b2e16bf2739f54870fa7fbe0d09797
Thyagarajan Kal <110703270+Thyagss@users.noreply.github.com>
2026-06-21 01:50:53 +0530

    practised diamond pattern

---
+++ patterns/DiamondStar.java
@@ -0,0 +1,53 @@
+/*
+
+Sample Output:
+if input = 6,
+
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+
+*/
+
+package patterns;
+
+import java.util.Scanner;
+
+public class DiamondStar {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int k=1; k<=(i*2)-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=1; j<= n-i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int m=1; m<=(i*2)-1; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
