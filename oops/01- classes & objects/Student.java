0c0cb19570cbcc8afb91b38da3dfb425679720fe
Thyagarajan Kal <110703270+Thyagss@users.noreply.github.com>
2026-07-10 23:55:37 +0530

    Implement constructors in Student class
    Added no-args and parameterized constructors to Student class.

--- oops/01- classes & objects/Student.java
+++ oops/01- classes & objects/Student.java
@@ -8,6 +8,21 @@ class Student {
     int age;
     double cgpa;
 
+    // No-Args Constructor
+    Student() {
+        System.out.println("Student Object Created!");
+    }
+
+    // Parameterized Constructor
+    Student(String name, String department, int age, double cgpa) {
+
+    this.name = name;
+    this.department = department;
+    this.age = age;
+    this.cgpa = cgpa;
+
+    System.out.println("Student Object Created using Parameterized Constructor!");
+    }
     
     // Behaviours
     void study() {
