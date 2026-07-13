128063a1044579c253e1ce6615588fd64bf4fb55
Thyagarajan Kal <110703270+Thyagss@users.noreply.github.com>
2026-07-13 22:55:26 +0530

    Add ConstructorChaining class

---
+++ oops/02 - constructors/ConstructorChainingDemo.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class ConstructorChainingDemo {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Thyags", "CSE", 22, 8.25);
+
+        student.displayStudent();
+    }
+}
